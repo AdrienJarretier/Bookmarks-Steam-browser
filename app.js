543e40704923915bba1d053ab0d377c1b88d77d1
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2020-05-21 21:14:31 +0200

    removed cookie parser

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var createError = require('http-errors');
 var express = require('express');
 
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var passport = require('passport');
@@ -106,7 +105,6 @@ app.use(passport.session());
 // app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
