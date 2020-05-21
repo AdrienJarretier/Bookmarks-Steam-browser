7b921f6b2dde1e9ccd645e29413e77e51b1b4d78
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2020-05-21 21:37:23 +0200

    logger set to debug

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
